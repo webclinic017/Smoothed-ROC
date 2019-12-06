33bb0f16c4a15d467cf5823ca9a084f0f70669ce
codebiscuits <58611002+codebiscuits@users.noreply.github.com>
2019-12-06 22:07:16 +0000

    Create data_1m.py

---
+++ data_1m.py
@@ -0,0 +1,14 @@
+import keys
+import datetime
+from binance.client import Client
+import pandas as pd
+
+client = Client(keys.Pkey, keys.Skey)
+symbol = 'BTCUSDT'
+klines = client.get_historical_klines(symbol, Client.KLINE_INTERVAL_1MINUTE, "1 Jan, 2017")
+
+BTC = pd.DataFrame(klines, columns=['Open Time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Close Time', 'Quote Asset Volume', 'Number of Trades', 'Taker Buy Base Asset Volume', 'Taker Buy Quote Asset Volume', 'Ignore'])
+BTC['Open Time'] = pd.to_datetime(BTC['Open Time'], unit='ms')
+BTC.set_index('Open Time', inplace=True)
+
+BTC.to_csv('BTC_1m_'+str(datetime.datetime.now().strftime("%Y_%m_%d")), date_format='%Y-%m-%d %H:%M:%S')
